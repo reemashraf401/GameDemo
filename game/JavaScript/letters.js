00685f632a240891d84ca9bdc414a77a7234196c
[reem] <[reemashraf180@gmail.com]>
2022-11-22 18:26:31 +0200

    edit letters.json

--- game/JavaScript/letters.js
+++ game/JavaScript/letters.js
@@ -33,15 +33,16 @@
 //         "skills" : ["football"]
 //     }
 // ]
-var student = 
-[{"id":1,"email":"george.bluth@reqres.in","first_name":"George","last_name":"Bluth","avatar":"apple.jpg","audio":"a.mp3","enable":"false"},
-{"id":2,"email":"janet.weaver@reqres.in","first_name":"Janet","last_name":"Weaver","avatar":"noon-logo-en.svg","audio":"child.mp3","enable":"false"}
-]
+import letter from './letters.json' assert {type : 'json'};
+// var student = 
+// [{"id":1,"email":"george.bluth@reqres.in","first_name":"George","last_name":"Bluth","avatar":"apple.jpg","audio":"a.mp3","enable":"false"},
+// {"id":2,"email":"janet.weaver@reqres.in","first_name":"Janet","last_name":"Weaver","avatar":"noon-logo-en.svg","audio":"child.mp3","enable":"false"}
+// ]
 var Arrayofaudio = new Array();
 var divs = document.getElementById("all");
 var Arrayofimage = new Array();
 var i;
-for(i=0;i<student.length;i++){
+for(i=0;i<letter.letters.length;i++){
     Arrayofimage[i] = document.createElement("img");
     Arrayofaudio[i] = document.createElement("audio");
     Arrayofimage[i] .addEventListener("click", playA(i))
@@ -49,9 +50,9 @@ for(i=0;i<student.length;i++){
     divs.appendChild(Arrayofaudio[i]);
 }
 
-for(var i=0;i<student.length;i++){
-    Arrayofimage[i].src = student[i]["avatar"]
-    Arrayofaudio[i].src = student[i]["audio"]
+for(var i=0;i<letter.letters.length;i++){
+    Arrayofimage[i].src = letter.letters[i]["imageSrc"]
+    Arrayofaudio[i].src = letter.letters[i]["audioSrc"]
     console.log(Arrayofaudio[i])
 }
 function playA(img){
