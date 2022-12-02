8918f606edecab8aa4d3483d7be425e44314292f
reemashraf401 <reemashraf180@gmail.com>
2022-12-02 16:16:57 +0200

    all

--- JavaScript/words.js
+++ JavaScript/words.js
@@ -6,15 +6,12 @@ var i;
 for(i=0;i<word.words.length;i++){
     Arrayofimage[i] = document.createElement("img");
     Arrayofaudio[i] = document.createElement("audio");
+    Arrayofimage[i].src = word.words[i]["imageSrc"]
+    Arrayofaudio[i].src = word.words[i]["audioSrc"]
+    Arrayofaudio[i].type="audio/mpeg"
     Arrayofimage[i] .addEventListener("click", playA(i))
     divs.appendChild(Arrayofimage[i]);
     divs.appendChild(Arrayofaudio[i]);
-}
-
-for(var i=0;i<word.words.length;i++){
-    Arrayofimage[i].src = word.words[i]["imageSrc"]
-    Arrayofaudio[i].src = word.words[i]["audioSrc"]
-    console.log(Arrayofimage[i])
 }
 function playA(img){
     return function() {
@@ -22,13 +19,4 @@ function playA(img){
         Arrayofaudio[img].play();
       }
    
-    // .currentTarget.style.opacity=0.0;
-    }
-// Pause and play the video, and change the button text
-//   if (video.paused) {
-//     video.pause();
-//     btn.innerHTML = "Pause";
-//   } else {
-//     video.pause();
-//     btn.innerHTML = "Play";
-//   }
+}
